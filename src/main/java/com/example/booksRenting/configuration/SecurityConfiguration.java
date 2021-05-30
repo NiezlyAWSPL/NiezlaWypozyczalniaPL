322a03892649f024de8528b0c20f51dbfe0cb12b
stefano <99xkubax99@gmail.com>
2021-05-30 13:52:22 +0200

    added cognito

---
+++ src/main/java/com/example/booksRenting/configuration/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package com.example.booksRenting.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .and()
+                .authorizeRequests(authz -> authz.mvcMatchers("/")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .oauth2Login()
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+                        System.out.println("Siema, zalogowales sie, " + authentication.getPrincipal().toString());
+                    }
+                })
+                .and()
+                .logout()
+                .logoutSuccessUrl("/");
+    }
+}
