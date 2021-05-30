7392c62b69a2d65220b8b899b63fb547f0a28373
Przemyslaw Wcislo <pwcislo@ittouch.com>
2021-05-30 18:05:47 +0200

    add blank services and components

--- niezly-frontend/src/app/app.module.ts
+++ niezly-frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { RentalsComponent } from './rentals/rentals.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    RentalsComponent
   ],
   imports: [
     BrowserModule
