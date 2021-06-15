c42621b006e82d786c6bc2ef17b4a2b9e5543ad9
Jakub Stefaniuk <jstefaniuk@ittouch.com>
2021-06-15 23:51:21 +0200

    reservation service

--- niezly-frontend/src/app/service/reservation.service.ts
+++ niezly-frontend/src/app/service/reservation.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {BookDTO, ReserveBookRequestDTO} from "../dto/dto";
 
 @Injectable({
     providedIn: 'root'
@@ -8,4 +10,15 @@ export class ReservationService {
 
     constructor(private http: HttpClient) { }
 
+    reserveBook(request: ReserveBookRequestDTO): Observable<BookDTO> {
+        return this.http.post<BookDTO>(`/api/books/reserved`, request);
+    }
+
+    getReservations(user: string): Observable<BookDTO[]> {
+        return this.http.get<BookDTO[]>(`/api/books/reserved`, { params: new HttpParams().set('user', user) });
+    }
+
+    cancelReservation(pk: number): Observable<BookDTO> {
+        return this.http.delete<BookDTO>(`/api/books/reserved${pk}`);
+    }
 }
