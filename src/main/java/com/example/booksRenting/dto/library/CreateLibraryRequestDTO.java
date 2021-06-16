9e877b3cb3d7d52155c149f828bc12758631ee0c
Jakub Stefaniuk <jstefaniuk@ittouch.com>
2021-06-16 16:12:02 +0200

    mocked libraries & books

--- src/main/java/com/example/booksRenting/dto/library/CreateLibraryRequestDTO.java
+++ src/main/java/com/example/booksRenting/dto/library/CreateLibraryRequestDTO.java
@@ -1,8 +1,12 @@
 package com.example.booksRenting.dto.library;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateLibraryRequestDTO {
     private String name;
     private String address;
--- src/main/java/com/example/booksRenting/repository/LibraryRepository.java
+++ src/main/java/com/example/booksRenting/repository/LibraryRepository.java
@@ -2,10 +2,12 @@ package com.example.booksRenting.repository;
 
 import com.example.booksRenting.model.entity.BaseEntity;
 import com.example.booksRenting.model.entity.Library;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
+@EnableScan
 public interface LibraryRepository extends PagingAndSortingRepository<Library, BaseEntity.BaseEntityId> {
     List<Library> findByPk(String pk);
 }
