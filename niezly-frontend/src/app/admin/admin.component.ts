10d5960d83b4687180828500b5a0982f98f0f279
Przemyslaw Wcislo <pwcislo@ittouch.com>
2021-06-16 23:53:46 +0200

    admin component functionality

--- niezly-frontend/src/app/admin/admin.component.ts
+++ niezly-frontend/src/app/admin/admin.component.ts
@@ -1,40 +1,61 @@
-import {Component, OnInit} from '@angular/core';
-import {BookDTO, RentalDTO, UserDTO} from "../dto/dto";
+import {Component} from '@angular/core';
+import {BookDTO, RentalDTO, RentBookRequestDTO, ReturnBookRequestDTO} from "../dto/dto";
 import {RentalService} from "../service/rental.service";
 import {UserService} from "../service/user.service";
+import {ReservationService} from "../service/reservation.service";
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent {
 
   constructor(private userService: UserService,
+              private reservationService: ReservationService,
               private rentalService: RentalService) {
   }
 
-  userLogin: string;
-  rentals: RentalDTO[];
-  reservations: BookDTO[];
+  login: string = "";
+
+  rentals: RentalDTO[] = [];
+  reservations: BookDTO[] = [];
+
+  selectedRental: RentalDTO;
+  selectedReservation: BookDTO;
 
   showBookRentalForm: boolean = false;
-  showBookReturnForm: boolean = false;
+  showBookReturnDialog: boolean = false;
+  showReservationCancelDialog: boolean = false;
 
-  ngOnInit(): void {
-    this.rentals = this.reservations = [];
+  loadUserData(userLogin: string) {
+    this.login = userLogin;
+    this.reservationService.getReservations(userLogin).subscribe(reservations => this.reservations = reservations);
+    this.rentalService.getUserOldRentals(userLogin).subscribe(rentals => this.rentals = rentals);
   }
 
-  loadUserData() {
-    console.log(this.userLogin);
+  openReturnDialog(rental: RentalDTO) {
+    this.selectedRental = rental;
+    this.showBookReturnDialog = true;
   }
 
-  openManualRentalDialog() {
+  openRentalDialog() {
     this.showBookRentalForm = true;
   }
 
-  onRentalYesClick(bookTitle: string) {
-    this.showBookRentalForm = false;
+  openReservationCancelDialog(reservation: BookDTO) {
+    this.selectedReservation = reservation;
+    this.showReservationCancelDialog = true;
+  }
+
+  onRentalYesClick(bookId: string) {
+    const rentBookRequest = new RentBookRequestDTO();
+    rentBookRequest.pk = bookId;
+    rentBookRequest.user = this.login;
+
+    this.rentalService.rentBook(rentBookRequest).subscribe(book => {
+      this.showBookRentalForm = false;
+    });
   }
 
   onRentalNoClick() {
@@ -42,10 +63,28 @@ export class AdminComponent implements OnInit {
   }
 
   onReturnYesClick() {
-    this.showBookReturnForm = false;
+    const returnBookRequest = {pk: this.selectedRental.pk};
+
+    this.rentalService.returnBook(returnBookRequest).subscribe(_ => {
+      this.showBookReturnDialog = false;
+      this.selectedRental = null;
+    })
   }
 
   onReturnNoClick() {
-    this.showBookReturnForm = false;
+    this.showBookReturnDialog = false;
+    this.selectedRental = null;
+  }
+
+  onCancelReservationNoClick() {
+    this.showReservationCancelDialog = false;
+    this.selectedReservation = null;
+  }
+
+  onCancelReservationYesClick() {
+    this.reservationService.cancelReservation(this.selectedReservation.pk).subscribe(_ => {
+      this.showReservationCancelDialog = false;
+      this.selectedReservation = null;
+    });
   }
 }
