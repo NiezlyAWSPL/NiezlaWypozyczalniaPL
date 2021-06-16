cf8f1e89629e309a07bc007612674ff13935feef
Przemyslaw Wcislo <pwcislo@ittouch.com>
2021-06-16 15:14:56 +0200

    book and user management components

--- niezly-frontend/src/app/app.routes.ts
+++ niezly-frontend/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import {Routes} from '@angular/router';
 import {BooksComponent} from "./books/books.component";
 import {RentalsComponent} from "./rentals/rentals.component";
 import {MainComponent} from "./main/main.component";
+import {BookManagementComponent} from "./book-management/book-management.component";
+import {UserManagementComponent} from "./user-management/user-management.component";
 
 export const routes: Routes = [
     {
@@ -15,6 +17,14 @@ export const routes: Routes = [
             {
                 path: 'books',
                 component: BooksComponent
+            },
+            {
+                path: 'book/management',
+                component: BookManagementComponent
+            },
+            {
+                path: 'user/management',
+                component: UserManagementComponent
             }
         ]
     },
