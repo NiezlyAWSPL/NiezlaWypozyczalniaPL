b898573eacb380a1a183e3826778d3f168be87db
Jakub Stefaniuk <jstefaniuk@ittouch.com>
2021-06-17 00:38:57 +0200

    :O

--- src/main/java/com/example/booksRenting/utils/AuthorizationUtils.java
+++ src/main/java/com/example/booksRenting/utils/AuthorizationUtils.java
@@ -4,12 +4,14 @@ import com.example.booksRenting.exception.NotABibliotekarzException;
 import lombok.experimental.UtilityClass;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import static com.example.booksRenting.constants.RoleConstants.NIEZLY_BIBLIOTEKARZ;
+
 @UtilityClass
 public class AuthorizationUtils {
 
     public static final void assureIsNiezlyBibliotekarz() {
         SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream()
-                .filter(a -> a.equals("SCOPE_books-renting/librarian"))
+                .filter(a -> a.getAuthority().contains(NIEZLY_BIBLIOTEKARZ))
                 .findFirst()
                 .orElseThrow(() -> new NotABibliotekarzException("Totally not a kurwa bibliotekarz B)"));
     }
